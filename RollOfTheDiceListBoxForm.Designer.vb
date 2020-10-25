941e1af32ec93e3a1b3be139a98a64d24814f922
harmdavi <harmondrum@gmail.com>
2020-10-24 22:01:34 -0600

    Add project files.

---
+++ RollOfTheDiceListBoxForm.Designer.vb
@@ -0,0 +1,120 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class RollOfTheDiceListBoxForm
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.RollTheDiceBox = New System.Windows.Forms.ListBox()
+        Me.ExitButton = New System.Windows.Forms.Button()
+        Me.ClearButton = New System.Windows.Forms.Button()
+        Me.RollButton = New System.Windows.Forms.Button()
+        Me.MenuStrip2 = New System.Windows.Forms.MenuStrip()
+        Me.MenuToolStripMenuItem1 = New System.Windows.Forms.ToolStripMenuItem()
+        Me.HelpToolStripMenuItem1 = New System.Windows.Forms.ToolStripMenuItem()
+        Me.MenuStrip2.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'RollTheDiceBox
+        '
+        Me.RollTheDiceBox.FormattingEnabled = True
+        Me.RollTheDiceBox.ItemHeight = 25
+        Me.RollTheDiceBox.Location = New System.Drawing.Point(28, 71)
+        Me.RollTheDiceBox.Name = "RollTheDiceBox"
+        Me.RollTheDiceBox.Size = New System.Drawing.Size(684, 204)
+        Me.RollTheDiceBox.TabIndex = 0
+        '
+        'ExitButton
+        '
+        Me.ExitButton.Location = New System.Drawing.Point(585, 331)
+        Me.ExitButton.Name = "ExitButton"
+        Me.ExitButton.Size = New System.Drawing.Size(171, 94)
+        Me.ExitButton.TabIndex = 1
+        Me.ExitButton.Text = "Exit"
+        Me.ExitButton.UseVisualStyleBackColor = True
+        '
+        'ClearButton
+        '
+        Me.ClearButton.Location = New System.Drawing.Point(360, 331)
+        Me.ClearButton.Name = "ClearButton"
+        Me.ClearButton.Size = New System.Drawing.Size(171, 94)
+        Me.ClearButton.TabIndex = 2
+        Me.ClearButton.Text = "Clear"
+        Me.ClearButton.UseVisualStyleBackColor = True
+        '
+        'RollButton
+        '
+        Me.RollButton.Location = New System.Drawing.Point(106, 331)
+        Me.RollButton.Name = "RollButton"
+        Me.RollButton.Size = New System.Drawing.Size(171, 94)
+        Me.RollButton.TabIndex = 3
+        Me.RollButton.Text = "Roll"
+        Me.RollButton.UseVisualStyleBackColor = True
+        '
+        'MenuStrip2
+        '
+        Me.MenuStrip2.GripMargin = New System.Windows.Forms.Padding(2, 2, 0, 2)
+        Me.MenuStrip2.ImageScalingSize = New System.Drawing.Size(32, 32)
+        Me.MenuStrip2.Items.AddRange(New System.Windows.Forms.ToolStripItem() {Me.MenuToolStripMenuItem1, Me.HelpToolStripMenuItem1})
+        Me.MenuStrip2.Location = New System.Drawing.Point(0, 0)
+        Me.MenuStrip2.Name = "MenuStrip2"
+        Me.MenuStrip2.Size = New System.Drawing.Size(915, 48)
+        Me.MenuStrip2.TabIndex = 7
+        Me.MenuStrip2.Text = "MenuStrip2"
+        '
+        'MenuToolStripMenuItem1
+        '
+        Me.MenuToolStripMenuItem1.Name = "MenuToolStripMenuItem1"
+        Me.MenuToolStripMenuItem1.Size = New System.Drawing.Size(98, 40)
+        Me.MenuToolStripMenuItem1.Text = "Menu"
+        '
+        'HelpToolStripMenuItem1
+        '
+        Me.HelpToolStripMenuItem1.Name = "HelpToolStripMenuItem1"
+        Me.HelpToolStripMenuItem1.Size = New System.Drawing.Size(85, 40)
+        Me.HelpToolStripMenuItem1.Text = "Help"
+        '
+        'RollOfTheDiceListBoxForm
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(12.0!, 25.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(915, 589)
+        Me.Controls.Add(Me.MenuStrip2)
+        Me.Controls.Add(Me.RollButton)
+        Me.Controls.Add(Me.ClearButton)
+        Me.Controls.Add(Me.ExitButton)
+        Me.Controls.Add(Me.RollTheDiceBox)
+        Me.Name = "RollOfTheDiceListBoxForm"
+        Me.Text = "Roll of The Dice"
+        Me.MenuStrip2.ResumeLayout(False)
+        Me.MenuStrip2.PerformLayout()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents RollTheDiceBox As ListBox
+    Friend WithEvents ExitButton As Button
+    Friend WithEvents ClearButton As Button
+    Friend WithEvents RollButton As Button
+    Friend WithEvents MenuStrip2 As MenuStrip
+    Friend WithEvents MenuToolStripMenuItem1 As ToolStripMenuItem
+    Friend WithEvents HelpToolStripMenuItem1 As ToolStripMenuItem
+End Class
---
+++ RollOfTheDiceListBoxForm.vb
@@ -0,0 +1,98 @@
+﻿
+'David Harmon
+'RCET0265
+'Spring 2020
+'Roll of the Dice List Box Form
+'
+
+Option Strict On
+Option Explicit On
+Option Compare Text
+
+
+Public Class RollOfTheDiceListBoxForm
+    Private Sub RollTheDice()
+        Dim diceArray(12) As Integer
+        Dim randomNumber As Integer
+        Dim value As Integer = 0
+        Dim intI As Integer
+        Dim iterations As Integer
+        Dim userInput As String
+        Dim Text As String
+        Dim Text2 As String
+        Dim dashLines As String
+
+
+
+        Do Until userInput = "q"
+
+            Randomize()
+
+
+            For i = 0 To 1000
+                randomNumber = CInt((12 - 2 + 1) * Rnd() + 2)
+
+                ' The way that the randomizer equastion works, there need to be code in there to make sure that everthing is rounded up. 
+                'This ensures that the random number always falls into working order. 
+
+                If diceArray(randomNumber - 2) < 2 Then
+                    i = 2
+                ElseIf (randomNumber - 2) > 13 Then
+                    i = 12
+                End If
+
+                'End If
+
+                diceArray(randomNumber - 2) += 1
+
+            Next
+
+            'This part of the code formats the code so that it is easy to understand by the user. 
+            For i = 2 To 12
+                Text = Text & String.Format($"{i} |{vbTab}")
+            Next
+            RollTheDiceBox.Items.Add("Numbers:        " & Text)
+            dashLines = (StrDup(177, "-"))
+            RollTheDiceBox.Items.Add(dashLines)
+
+            For i = 0 To 10
+                Text2 = (Text2 & String.Format($"{0,10}", i) & "|")
+            Next
+
+            RollTheDiceBox.Items.Add("TimesRolled" & Text2)
+            RollTheDiceBox.Items.Add(dashLines)
+            RollTheDiceBox.Items.Add("")
+            RollTheDiceBox.Items.Add("")
+
+
+
+            'For i = 0 To 10
+            '    dashLines = ("--------")
+            'Next
+
+
+            'For i = 0 To 10
+            '    'Console.Write(i)
+            '    Text2 = String.Format($"{diceArray(i)} |{vbTab}")
+
+            'Next
+
+            'RollTheDiceBox.Items.Add("Times Rolled:" & Text2)
+            'RollTheDiceBox.Items.Add(dashLines)
+            'RollTheDiceBox.Items.Add("")
+            'RollTheDiceBox.Items.Add("")
+
+            'Console.Clear()
+            ''This part clears the array to all 0's so that the ranomizer can load another 1000
+            'For intI = 0 To 12
+            '    diceArray(intI) = 0
+            'Next
+
+        Loop
+
+    End Sub
+
+    Private Sub RollButton_Click(sender As Object, e As EventArgs) Handles RollButton.Click
+        RollTheDice()
+    End Sub
+End Class
